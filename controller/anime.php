beb036f39eee58557e90c383adc3f4d07eb68d00
Kyrozyn <arizkirizaldi@email.unikom.ac.id>
2019-05-31 21:42:24 +0700

    testing nim comand

--- controller/anime.php
+++ controller/anime.php
@@ -6,6 +6,12 @@ namespace Controller;
 
 use Exception;
 use Jikan\Jikan;
+use LINE\LINEBot\MessageBuilder\Flex\ComponentBuilder\TextComponentBuilder;
+use LINE\LINEBot\MessageBuilder\Flex\ContainerBuilder\BubbleContainerBuilder;
+use LINE\LINEBot\MessageBuilder\FlexMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder;
 use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuilder;
 use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselTemplateBuilder;
 use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
@@ -28,6 +34,7 @@ class anime
     {
         $reply = null;
         $halaman = $page;
+        $nextpage = $page+1;
         $batas = 4;
         if (empty($halaman)) {
             $posisi = 0;
@@ -36,18 +43,16 @@ class anime
             $posisi = ($halaman - 1) * $batas;
         }
         $result = $this->jikan->AnimeSearch($title)->getResults();
-        try {
             $Carousel = new ImageCarouselTemplateBuilder([
                 new ImageCarouselColumnTemplateBuilder($result[$posisi]->getImageUrl(), new UriTemplateActionBuilder(substr($result[$posisi]->getTitle(),0,11), $result[$posisi]->getUrl())),
                 new ImageCarouselColumnTemplateBuilder($result[$posisi + 1]->getImageUrl(), new UriTemplateActionBuilder(substr($result[$posisi + 1]->getTitle(),0,11), $result[$posisi + 1]->getUrl())),
                 new ImageCarouselColumnTemplateBuilder($result[$posisi + 2]->getImageUrl(), new UriTemplateActionBuilder(substr($result[$posisi+2]->getTitle(),0,11), $result[$posisi+2]->getUrl())),
                 new ImageCarouselColumnTemplateBuilder($result[$posisi + 3]->getImageUrl(), new UriTemplateActionBuilder(substr($result[$posisi+3]->getTitle(),0,11), $result[$posisi+3]->getUrl())),
-                new ImageCarouselColumnTemplateBuilder("https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png", new MessageTemplateActionBuilder("Next", "nim"))
+                new ImageCarouselColumnTemplateBuilder("https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png", new MessageTemplateActionBuilder("Next", "nim*".$title.$nextpage))
             ]);
             $reply = new TemplateMessageBuilder("Anime", $Carousel);
-        } catch (Exception $e) {
-            file_put_contents('php://stderr', 'Exception : ' . $e->getMessage());
-        }
+            $header = new TextComponentBuilder($result[$posisi]->getTitle(),null,null,"md",null,null,true);
+            $bubble = new BubbleContainerBuilder(null,$header);
         return $reply;
     }
 }
