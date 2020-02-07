e771c08e2bb1e76492010d8a4d26c5d79ae6576c
Andreas <arizkirizaldi@email.unikom.ac.id>
2020-02-07 14:44:35 +0700

    heroku cred

--- model/AobjectDB.php
+++ model/AobjectDB.php
@@ -20,11 +20,11 @@ class AobjectDB
         try {
             $this->db = new Medoo([
                 'database_type' => 'mysql',
-                'database_name' => 'ririla',
-                'server'        => 'localhost',
+                'database_name' => 'heroku_8936a7e2cfac410',
+                'server'        => 'us-cdbr-iron-east-04.cleardb.net',
                 'port'          => '3306',
-                'username'      => 'root',
-                'password'      => '',
+                'username'      => 'b8576eef192d88',
+                'password'      => 'cd90f92c',
             ]);
         } catch (Exception $e) {
             debug::debugToMe($e->getMessage());
