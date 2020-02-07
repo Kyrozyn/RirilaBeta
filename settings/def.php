bb5bdb4e54d54d2364e7ccb1f98113ac0261f53b
Andreas <arizkirizaldi@email.unikom.ac.id>
2020-02-07 17:06:16 +0700

    dynamic credentials

--- settings/def.php
+++ settings/def.php
@@ -1,5 +1,5 @@
 <?php
 
 //define channel line
-$channel_access_token = '1qwJywd0S23JVOyeJ1DRN3pdNwGfwnVFjMS3bVqXY3SgvWPaoIrA1vlZlwux13q6kyewnq5tunMkzi+RUnlSDMiWnL/jZQDK8MNDo2A/4b/24AOfBhaC4sQEdrJRhgbdFM0JRA5hb0Vt9kQIPRVUPAdB04t89/1O/w1cDnyilFU=';
-$channel_secret = '093cf2a84ff98c270a5c5887e9fb9402';
+$channel_access_token = getenv("CHANNEL_ACCESS_TOKEN") ? getenv("DATABASE_NAME") : "";
+$channel_secret = getenv("CHANNEL_SECRET") ? getenv("CHANNEL_SECRET") : "";
