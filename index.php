e3181904a89dad12cb8d346af5e829dce8eb6808
kyrozyn <arizkirizaldi@email.unikom.ac.id>
2020-02-16 23:00:20 +0700

    add header api

--- index.php
+++ index.php
@@ -176,7 +176,7 @@ $app->get('/getstatskeywords', function ($req, Response $res) {
     $api = new api();
     $keywords = $api->getStatsKeyword();
     return $res->withJson($keywords)
-        ->withHeader('Access-Control-Allow-Origin', 'http://localhost:8000')
+        ->withHeader('Access-Control-Allow-Origin', '*')
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
