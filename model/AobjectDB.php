edee25a554bd787bc111994316deaf554af7b793
Andreas <arizkirizaldi@email.unikom.ac.id>
2020-02-19 20:49:20 +0700

    Revert "change to env"
    This reverts commit 9e96e1ed

--- model/AobjectDB.php
+++ model/AobjectDB.php
@@ -20,11 +20,11 @@ class AobjectDB
         try {
             $this->db = new Medoo([
                 'database_type' => 'mysql',
-                'database_name' => getenv('DATABASE_NAME'),
+                'database_name' => 'ririla',
                 'server'        => '127.0.0.1',
-                'port'          => getenv('DATABASE_PORT'),
-                'username'      => getenv('DATABASE_USERNAME'),
-                'password'      => getenv('DATABASE_PASSWORD'),
+                'port'          => '57181',
+                'username'      => 'azure',
+                'password'      => '6#vWHD_$',
             ]);
             $this->db->query("SET GLOBAL event_scheduler='ON'");
         } catch (Exception $e) {
