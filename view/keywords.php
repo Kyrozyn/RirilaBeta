d1bd2d162ae88dd18ff9bdd41761b888b6be01e5
Andreas <arizkirizaldi@email.unikom.ac.id>
2020-03-08 17:16:02 +0700

    move view

---
+++ view/keywords.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Ririna</title>
+    <link rel="icon" type="image/jpeg" sizes="640x640" href="assets/img/astoru-ava.jpg">
+    <link rel="icon" type="image/jpeg" sizes="640x640" href="assets/img/astoru-ava.jpg">
+    <link rel="icon" type="image/jpeg" sizes="640x640" href="assets/img/astoru-ava.jpg">
+    <link rel="icon" type="image/jpeg" sizes="640x640" href="assets/img/astoru-ava.jpg">
+    <link rel="icon" type="image/jpeg" sizes="1080x868" href="assets/img/astoru-header.jpg">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css"
+          crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <style>
+        table {
+            table-layout: fixed;
+            font-size: x-small;
+        }
+        tr{
+            width: 20px;
+        }
+    </style>
+</head>
+
+<body>
+<nav class="navbar navbar-light navbar-expand-md navigation-clean">
+    <div class="container"><a class="navbar-brand" href="#">Kelola Keyword(s) Riri(la)</a></div>
+</nav>
+
+<div class="container">
+    <div class="table-responsive">
+        <table id="example" class="table table-striped table-bordered table-sm">
+            <thead>
+            <tr>
+                <th>Keywords yang tersedia</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Tiger Nixon</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
